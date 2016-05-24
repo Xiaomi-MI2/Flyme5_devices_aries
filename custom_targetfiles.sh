2d4483d2c7eef66c1765c5953d5ae670b12cfb91
bywwh <1065449148@qq.com>
2016-05-24 20:46:37 +0800

    Update

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -1,21 +1,8 @@
 echo ">>> in $0"
-OUT_DIR=/home/bywwh/FlymeOS/devices/aries/out/merged_target_files
-OTHER_DIR=/home/bywwh/FlymeOS/devices/aries/other
+OUT_DIR=out/merged_target_files
 
-cd $OUT_DIR
-echo
-echo ">>> delete $OUT_DIR/DATA"
-echo
-rm -v -rf DATA/
-echo
-echo ">>> delete $OUT_DIR/SYSTEM/priv-app/Settings/Settings"
-echo
-rm -v -rf $OUT_DIR/SYSTEM/priv-app/Settings/Settings
-echo
-echo ">>> create dir DATA/"
-echo
-mkdir -vp DATA/
-echo
-echo ">>> move some files into OUT_DIR"
-echo
-cp -v -rf $OTHER_DIR/data $OUT_DIR/DATA
+rm -v -rf $OUT_DIR/DATA
+mkdir -vp $OUT_DIR/DATA
+cp -v -rf other/data $OUT_DIR/DATA
+cp -v -rf other/supersu $OUT_DIR/META
+cp -v -rf other/file_contexts $OUT_DIR/META
